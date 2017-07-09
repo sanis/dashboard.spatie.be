16084f4d915564215a298b55642b3c91c6842acb
Justinas Bolys <justinas.bolys@nfq.com>
2017-07-09 20:18:44 +0300

    Clean dasboard layout.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,22 +5,9 @@
 @javascript(compact('pusherKey', 'pusherCluster', 'usingNodeServer'))
 
         <dashboard id="dashboard" columns="5" rows="3">
-            <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a3"></twitter>
+            <sentry-errors position="b1:d1"></sentry-errors>
 
-            <sentry-errors position="b1:c1"></sentry-errors>
-
-            <calendar position="b1:b2"></calendar>
-            <music position="c1:d1"></music>
-            <uptime position="b3"></uptime>
-
-            <tasks team-member="alex" position="c2"></tasks>
-            <tasks team-member="freek" position="d2"></tasks>
-            <tasks team-member="seb" position="c3"></tasks>
-            <tasks team-member="willem" position="d3"></tasks>
-
-            <time-weather position="e1" date-format="ddd DD/MM"></time-weather>
-            <packagist position="e2"></packagist>
-            <github position="e3"></github>
+            <time-weather position="e1" date-format="YY/DD/MM ddd"></time-weather>
 
             <internet-connection></internet-connection>
         </dashboard>
